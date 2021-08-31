e56c15bedb6e998146da9bef01f1ec962fae6ad4
pachpolkalyani <89258306+pachpolkalyani@users.noreply.github.com>
2021-08-31 13:49:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,14 @@
+# ARM provider block 
+provider "azurerm"{
+version = ">=2.0"
+features {}
+}
 
+# Terraform backend configuration block 
+terraform {
+backend "azurerm" {
+resorce_group_name = "rg-cloudquickpocs"
+storage_account_name = "ccpsazuretf0001"
+container_name = "ccpterrraformstatefile"
+key = "ccpsterraform.tfstate"
+}
